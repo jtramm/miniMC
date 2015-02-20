bae042ff613f3677b4d2826d193a700bdb81ff63
John Tramm <john.tramm@gmail.com>
2015-02-20 14:23:16 -0500

    dev

--- main.c
+++ main.c
@@ -9,137 +9,143 @@
 
 int main(void)
 {
-	unsigned int seed = time(NULL);
 
-	int n_particles = 1000;
-
-	int n_absorbed = 0;
-	int n_scattered = 0;
-
+	long n_particles = 100000000;
 	int global_tally[600] = {0};
 	int global_variance[600] = {0};
-	int local_tally[600] = {0};
-	int local_variance[600] = {0};
-	int particle_tally[600] = {0};
 
-	float mean[600] = {0};
-	float variance[600] = {0};
+	omp_set_num_threads(omp_get_num_procs());
+	
+	double start = omp_get_wtime();
 
-	// Loop over particles
-	for( int i = 0; i < n_particles; i++ )
+	#pragma omp parallel default(none) shared(n_particles, global_tally, global_variance)  
 	{
-		// Particle State
-		float x;
-		float direction;
-		int region;
-		int alive = 1;
+		int local_tally[600] = {0};
+		int local_variance[600] = {0};
+		int particle_tally[600] = {0};
+		unsigned int seed = 1337 + time(NULL) + omp_get_thread_num();
 
-		// Birth Particle Location
-		x = ( (float) rand_r(&seed) / RAND_MAX ) * 6.f;
-		if( x < 2.f )
-			region = 1;
-		else
-			region = 2;
-
-		while(alive)
+		#pragma omp for schedule(dynamic)
+		// Loop over particles
+		for( long i = 0; i < n_particles; i++ )
 		{
-			// Sample Particle Direction
-			direction = rand_r(&seed) % 2; 
+			// Particle State
+			float x;
+			float direction;
+			int region;
+			int alive = 1;
 
-			// Set XS's
-			float sigma_t;
-			float sigma_a;
-			if( region == 1 )
-			{
-				sigma_t = 1.f;
-				sigma_a = 0.5f;
-			}
+			// Birth Particle Location
+			x = ( (float) rand_r(&seed) / RAND_MAX ) * 6.f;
+			if( x < 2.f )
+				region = 1;
 			else
-			{
-				sigma_t = 1.5f;
-				sigma_a = 1.2f;
-			}
+				region = 2;
 
-			// Sample Flight Distance
-			float dist = -logf( (float) rand_r(&seed) / RAND_MAX  ) / sigma_t;
-
-			// Move Particle
-			if( region == 1 )
+			while(alive)
 			{
-				if( direction = 0 )
-					if( dist > x ) // Particle Escapes Left
-					{
-						alive = 0;
-						break;
-					}
-					else
-						if( dist > 2.f - x ) // Particle Travels 1 -> 2
-						{
-							x = 2.f;
-							region = 2;
-							break;
-						}
-			}
-			else
-			{
-				if( direction = 0 )
-					if( dist > x - 2.f ) // Particle Travels 2 -> 1
-					{
+				// Sample Particle Direction
+				direction = rand_r(&seed) % 2; 
 
-						x = 2.f;
-						region = 1;
-						break;
-					}
-					else
-						if( dist > 6.f - x ) // Particle Escapes Right
+				// Set XS's
+				float sigma_t;
+				float sigma_a;
+				if( region == 1 )
+				{
+					sigma_t = 1.f;
+					sigma_a = 0.5f;
+				}
+				else
+				{
+					sigma_t = 1.5f;
+					sigma_a = 1.2f;
+				}
+
+				// Sample Flight Distance
+				float dist = -logf( (float) rand_r(&seed) / RAND_MAX  ) / sigma_t;
+
+				// Move Particle
+				if( region == 1 )
+				{
+					if( direction = 0 )
+						if( dist > x ) // Particle Escapes Left
 						{
 							alive = 0;
 							break;
 						}
-			}
+						else
+							if( dist > 2.f - x ) // Particle Travels 1 -> 2
+							{
+								x = 2.f;
+								region = 2;
+								break;
+							}
+				}
+				else
+				{
+					if( direction = 0 )
+						if( dist > x - 2.f ) // Particle Travels 2 -> 1
+						{
+
+							x = 2.f;
+							region = 1;
+							break;
+						}
+						else
+							if( dist > 6.f - x ) // Particle Escapes Right
+							{
+								alive = 0;
+								break;
+							}
+				}
 
 
-			// Sample Interaction
-			float interaction = (float) rand_r(&seed) / RAND_MAX * sigma_t;
-			if( interaction < sigma_a )
-			{
-				n_absorbed++;
-				alive = 0;
-				break;
+				// Sample Interaction
+				float interaction = (float) rand_r(&seed) / RAND_MAX * sigma_t;
+				if( interaction < sigma_a )
+				{
+					alive = 0;
+					break;
+				}
+				else
+					particle_tally[(int) (x*100)]++;
 			}
-			else
+
+			// Accumulate Tallies for Neutron history into Local running tally
+			for( int j = 0; j < 600; j++ )
 			{
-				n_scattered++; // also need to tally x location
-				particle_tally[(int) (x*100)]++;
+				local_tally[j] += particle_tally[j];
+				local_variance[j] += particle_tally[j] * particle_tally[j];
+				particle_tally[j] = 0;
 			}
+
 		}
 
-		// Accumulate Tallies for Neutron history into Local running tally
-		for( int j = 0; j < 600; j++ )
+		// Global Accumulation of Tallies
+		for( int i = 0; i < 600; i++ )
 		{
-			local_tally[j] += particle_tally[j];
-			local_variance[j] += particle_tally[j] * particle_tally[j];
-			particle_tally[j] = 0;
+			global_tally[i] = local_tally[i];
+			global_variance[i] = local_variance[i];
 		}
-
 	}
 
-	// Global Accumulation of Tallies
-	for( int i = 0; i < 600; i++ )
-	{
-		global_tally[i] = local_tally[i];
-		global_variance[i] = local_variance[i];
-	}
-
+	float mean[600] = {0};
+	float variance[600] = {0};
 	// Compute statistics
 	for( int i = 0; i < 600; i++ )
 	{
 		mean[i] = (float) global_tally[i] / n_particles;
 		variance[i] = sqrtf( 1.f / (n_particles-1) * ( (float) global_variance[i] / n_particles - mean[i] * mean[i] )  ); 
 	}
+
+//	for( int i = 0; i < 600; i ++ )
+//		printf("Bin: %d\tMean: %f\tVariance: %f\n", i, mean[i], variance[i]);
 	
-	for( int i = 0; i < 600; i ++ )
-		printf("Bin: %d\tMean: %f\tVariance: %f\n", i, mean[i], variance[i]);
+	double end = omp_get_wtime();
+
+	printf("Neutrons:   %g\n", (float) n_particles);
+	printf("Runtime:    %.3lf seconds\n", end-start);
+	printf("Neutrons/s: %g\n", n_particles/(end-start));
 
 	return 0;
 }
