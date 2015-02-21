08126cbeee78532b62eee1d97f7196731721113f
John Tramm <john.tramm@gmail.com>
2015-02-21 18:42:14 -0500

    added separate function inversion testing program for problem 2

---
+++ inv.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+
+int main(void)
+{
+	double val = 0;
+	long iterations = 10000000;
+	unsigned int seed = 1337 * (time(NULL) + 13);
+	for( int i = 0; i < iterations; i ++ )
+	{
+		double r = (double) rand_r(&seed) / RAND_MAX; 
+		double part = sqrt(16 * r*r - 16 * r + 5 ) + 4 *r - 2;
+		val += pow(part, 1.0/3.0) - pow(part,-1.0/3.0);
+	}
+
+	double average = val / iterations;
+
+	printf("Average = %lf\n", average);
+
+	return 0;
+}
